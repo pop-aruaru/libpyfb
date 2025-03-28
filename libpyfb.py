87764584c4a3e76d465db873fbe3cf8c5fa06053
Aruaru <4726806+pop-aruaru@users.noreply.github.com>
2025-03-28 14:45:46 +0900

    Update libpyfb.py
    Corrected address calculation for 3D axis

--- libpyfb.py
+++ libpyfb.py
@@ -73,7 +73,7 @@ class Framebuffer():
 
 			t: transparency (default set to 0)
 		'''
-		self.fb.seek(x*y*self.bpp//8) # Set the pixel location
+		self.fb.seek((x+(y*self.screeny))*self.bpp//8) # Set the pixel location
 
 		if self.bpp == 32:
 			# 32 bit per pixel
